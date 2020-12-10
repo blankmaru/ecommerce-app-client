4093a9801b00a5937b2b612604e726c01cf2fe18
blankmaru <coopskywavy@gmail.com>
2020-12-11 00:22:48 +0200

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Axios, { AxiosResponse } from 'axios';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [users, setUsers] = useState<Array<any>>([])
+
+  useEffect(() => {
+    Axios.get('http://46.101.154.231/api/users', {
+      withCredentials: true
+    })
+    .then((res: AxiosResponse) => {
+      setUsers(res.data)
+    })
+  }, [])
+
+  return (
+    <div>
+      {users.map((item) => <div>{item.username}</div>)}
+    </div>
+  );
+}
+
+export default App;
